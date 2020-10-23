393510d5836c41fe7dc4505a05b3553396c17523
nakamura999 <masahiro9ama9@gmail.com>
2020-10-24 00:42:12 +0900

    problem15 edit

--- Repetition/problem15.c
+++ Repetition/problem15.c
@@ -3,11 +3,8 @@ void main(){
    int b;
    for(a = 1; a <= 9; a++){
        for(b = 1; b <= 9; b++) {
-           if(b % 9 == 0){
-               printf("%d*%d=%d \n",b,a,a*b);            
-           } else {
-               printf("%d*%d=%d ",b,a,a*b);
-           }
+            printf("%d*%d=%d ",b,a,a*b);            
        }
+       printf("\n");
    }
 }
