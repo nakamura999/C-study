76708792e6a3e3daf8c93e0d13d2a5c1ce155a0b
nakamura999 <masahiro9ama9@gmail.com>
2020-10-28 00:10:30 +0900

    File problem1 add

---
+++ File/problem1/calulation.c
@@ -0,0 +1,11 @@
+#include "calulation.h"
+
+int div(int a, int b) {
+    int r = a / b;
+    return r;
+}
+
+int mod(int a, int b) {
+    int z = a % b;
+    return z;
+}
---
+++ File/problem1/calulation.h
@@ -0,0 +1,7 @@
+#ifndef _CALULATION_H_
+#define _CALULATION_H_
+
+int div(int,int);
+int mod(int,int);
+
+#endif // _CALULATION_H_
