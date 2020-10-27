5f335b53ee2d4dad169b7d87a5b2e48e4b2187c9
nakamura999 <masahiro9ama9@gmail.com>
2020-10-28 00:17:50 +0900

    File problem2 add

---
+++ File/problem2/randLib.c
@@ -0,0 +1,13 @@
+#include "randLib.h"
+#include <time.h>
+
+//  乱数の初期化
+void init_rand()
+{
+    srand((unsigned) time(NULL));
+}
+//  指定した範囲の乱数を発生
+int getRand(int start,int end)
+{
+    return rand() % (end - start + 1) + start;
+}
